08bb06e4967ab8bc37adf89480bffb913c8470b0
SILMI Hadjer <127608597+Gigi28gigi28@users.noreply.github.com>
2025-12-20 16:23:46 +0100

    Delete medinterships_backend directory

--- medinterships_backend/src/routes/evaluations.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { protect } = require('../middleware/auth');
-
-// Protected evaluations API routes could go here
-router.use(protect);
-
-module.exports = router;
